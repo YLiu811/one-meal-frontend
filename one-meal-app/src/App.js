b46383d19e26e9918a4528fd7cb3e2fe8ab4031b
Yun <ly.yun811@gmail.com>
2023-01-24 20:23:41 -0800

    Yun frontend Branch created. TBContinue

--- one-meal-app/src/App.js
+++ one-meal-app/src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Pages from "./pages/Pages";
 
 function App() {
+
+  const URL = "http://127.0.0.1:5000/user";
+  
   return (
     <div className="App">
       <header className="App-header">One-Meal</header>
--- one-meal-app/src/Components/Popular.jsx
+++ one-meal-app/src/Components/Popular.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function Popular() {
-    
+    const URL = "http://127.0.0.1:5000/user";
     return (
     <div>
       Popular Picks
