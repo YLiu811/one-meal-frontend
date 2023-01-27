527c3160636cc244c4a144eacfaf50d1a773afec
yangsu <suyang.sophia@outlook.com>
2023-01-26 20:38:29 -0800

    sign up and log in finished

--- one-meal-app/src/App.js
+++ one-meal-app/src/App.js
@@ -1,12 +1,65 @@
 import "./App.css";
-import Pages from "./pages/Pages";
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import Home from "./pages/Home";
+import NavBar from "./Components/NavBar";
+// import Pages from "./pages/Pages";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState({});
+  const fetchUser = () => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      console.log(`this is fetch `);
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/user/${userId}`)
+        .then((response) => {
+          console.log(response.data);
+          const userAPIResCopy = response.data.user;
+          setUser(userAPIResCopy);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  useEffect(fetchUser, []);
   return (
     <div className="App">
-      <header className="App-header">One-Meal</header>
-      <h1> One Meal </h1>
-      <Pages />
+      {/* <header className="App-header">One-Meal</header> */}
+      {/* <h1> One Meal </h1> */}
+      {/* <Pages /> */}
+
+      <NavBar userProp={user} setUser={setUser} />
+      <Routes>
+        <Route
+          path="/home"
+          element={<Home userProp={user} setUser={setUser} />}
+        />
+        <Route
+          path="/signup"
+          element={
+            user.user_id ? (
+              <Navigate to="/home" />
+            ) : (
+              <Signup userProp={user} setUser={setUser} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            user.user_id ? (
+              <Navigate to="/home" />
+            ) : (
+              <Login userProp={user} setUser={setUser} />
+            )
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ one-meal-app/src/Components/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = (props) => {
+  console.log("these are NavBar's props:", props);
+  return (
+    <nav>
+      {props.userProp.user_id ? (
+        <>
+          <h1>Hi! {props.userProp.email} </h1>
+          <span
+            onClick={() => {
+              localStorage.removeItem("userId");
+              props.setUser({});
+              alert("See you!");
+              window.location.href = "/login";
+            }}
+          >
+            Log out
+          </span>
+          <Link to="/home"></Link>
+        </>
+      ) : (
+        <>
+          <Link to="/signup">Sign up</Link>
+          <Link to="/login">Login</Link>
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
