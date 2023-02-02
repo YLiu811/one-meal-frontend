516470c5145291ffef660f6a92e750ca8a7a933b
Yun <ly.yun811@gmail.com>
2023-02-02 14:17:49 -0800

    Recipe page built. Functioning okay, need more content

--- one-meal-app/src/pages/Pages.jsx
+++ one-meal-app/src/pages/Pages.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Searched from './Searched';
+import Recipe from './Recipe';
 
 function Pages() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/searched/:input" element={<Searched />} />
+            <Route path="/recipe/:id" element={<Recipe />} />
         </Routes>
     );
 }
